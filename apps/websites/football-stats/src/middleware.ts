8cb324d167fafc3f9416ea12efc07679527dc972
Michael <michael@michaeljdfreelance.com>
2025-08-29 13:27:22 +0100

    Initial Commit

--- apps/websites/football-stats/src/middleware.ts
+++ apps/websites/football-stats/src/middleware.ts
@@ -1,12 +1,32 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware({
-    publicRoutes: ["/"],   // 👈 allow homepage
-} as any);
+const intlMiddleware = (req) => {
+    if (req.nextUrl.searchParams.get("bypass") === "S6sncMNU0Fy4o0b9") {
+        const res = NextResponse.next();
+        res.cookies.set("bypass", "true"); // 👈 drop a cookie
+        return res;
+    }
+    else {
+        const res = NextResponse.next();
+        res.cookies.delete("bypass"); // 👈 drop a cookie
+        return res;
+    }
+}
+
+const isProtectedRoute = createRouteMatcher(['dashboard/(.*)'])
+
+export default clerkMiddleware(async (auth, req) => {
+    if (isProtectedRoute(req)) await auth.protect()
+
+    return intlMiddleware(req)
+})
 
 export const config = {
     matcher: [
-        "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-        "/(api|trpc)(.*)",
+        // Skip Next.js internals and all static files, unless found in search params
+        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+        // Always run for API routes
+        '/(api|trpc)(.*)',
     ],
-};
+}
