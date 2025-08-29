46ec06b9e4094b30a31e8b069805ecf01e48ba6a
Michael <michael@michaeljdfreelance.com>
2025-08-29 15:24:16 +0100

    Initial Commit

--- apps/websites/football-stats/src/app/page.tsx
+++ apps/websites/football-stats/src/app/page.tsx
@@ -1,9 +1,30 @@
 import RadarExample from "@/components/Radar";
 import {TeamSelect} from "@/components/TeamSelect";
 import {Suspense} from "react";
-import {SignedIn} from "@clerk/nextjs";
+import {ClerkProvider, SignedIn} from "@clerk/nextjs";
+import {cookies} from "next/headers";
+import {Toaster} from "sonner";
 
 export default function Home() {
+    const bypass = (await cookies()).get("bypass")?.value;
+
+    if (bypass === "true") {
+        return (
+            <div className="bg-gray-50 font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+                <main className="w-full gap-[32px] row-start-2 items-center sm:items-start ">
+                    <Suspense fallback={<div>Loading teams...</div>}>
+                        <TeamSelect />
+                        <div className="p-4 w-full min-w-[300px] h-[300px] xl:min-w-[700px] xl:h-[700px] bg-white rounded-[20px] border">
+                            <RadarExample />
+                        </div>
+                    </Suspense>
+                </main>
+                <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+                </footer>
+            </div>
+        );
+    }
+
   return (
     <SignedIn>
         <div className="bg-gray-50 font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
