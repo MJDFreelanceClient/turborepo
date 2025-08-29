1c4cc1d27cefabbfd5b2bc25ac120ab29aa5aa3f
Michael <michael@michaeljdfreelance.com>
2025-08-29 12:27:35 +0100

    Initial Commit

--- apps/websites/football-stats/next.config.ts
+++ apps/websites/football-stats/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    eslint: {
+        // ❌ Don't run ESLint on build (only run it locally / CI if you want)
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
