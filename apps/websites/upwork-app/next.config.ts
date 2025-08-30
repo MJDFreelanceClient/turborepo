8e1037f85bd31dedcb87ee01162d053107af4382
Michael <michael@michaeljdfreelance.com>
2025-08-30 15:24:23 +0100

    Initial Commit

--- apps/websites/upwork-app/next.config.ts
+++ apps/websites/upwork-app/next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    eslint: {
+        // ❌ Don't run ESLint on build (only run it locally / CI if you want)
+        ignoreDuringBuilds: true,
+    },
   /* config options here */
 };
 
